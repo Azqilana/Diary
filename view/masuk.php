71024ddbd2c390370fb32a4f84843fe47b1450dc
azqilana <mazqulunu@gmail.com>
2021-09-19 07:38:42 +0700

    update login

--- view/masuk.php
+++ view/masuk.php
@@ -1,18 +1,34 @@
 <?php
-require('../proses/funtion.php');
-// session_start();
+session_start();
+require 'functions.php';
+
+// cek cookie
+if (isset($_COOKIE['id']) && isset($_COOKIE['key'])) {
+     $id = $_COOKIE['id'];
+     $key = $_COOKIE['key'];
+
+     // ambil username berdasarkan id
+     $result = mysqli_query($conn, "SELECT username FROM user WHERE id = $id");
+     $row = mysqli_fetch_assoc($result);
+
+     // cek cookie dan username
+     if ($key === hash('sha256', $row['username'])) {
+          $_SESSION['login'] = true;
+     }
+}
 
 if (isset($_SESSION["login"])) {
      header("Location: index.php");
      exit;
 }
 
+
 if (isset($_POST["login"])) {
 
      $username = $_POST["username"];
      $password = $_POST["password"];
 
-     $result = mysqli_query($conn, "SELECT * FROM user_login WHERE username = '$username'");
+     $result = mysqli_query($conn, "SELECT * FROM user WHERE username = '$username'");
 
      // cek username
      if (mysqli_num_rows($result) === 1) {
@@ -23,7 +39,14 @@ if (isset($_POST["login"])) {
                // set session
                $_SESSION["login"] = true;
 
-               header('Location:index.php');
+               // cek remember me
+               if (isset($_POST['remember'])) {
+                    // buat cookie
+                    setcookie('id', $row['id'], time() + 60);
+                    setcookie('key', hash('sha256', $row['username']), time() + 60);
+               }
+
+               header("Location: index.php");
                exit;
           }
      }
@@ -32,7 +55,6 @@ if (isset($_POST["login"])) {
 }
 
 ?>
-
 <!DOCTYPE html>
 <html lang="en">
 
