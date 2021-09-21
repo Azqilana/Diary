aedb7290eabfdcfb0cf9445824eb91431773b6f3
azqilana <mazqulunu@gmail.com>
2021-09-22 03:44:15 +0800

    session

--- view/logout.php
+++ view/logout.php
@@ -5,8 +5,6 @@ $_SESSION = [];
 session_unset();
 session_destroy();
 
-setcookie('id', '', time() - 3600);
-setcookie('key', '', time() - 3600);
 ob_start();
 header("Location: masuk.php");
 ob_end_flush();
